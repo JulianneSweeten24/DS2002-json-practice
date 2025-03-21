1be60fb6bca96f2fed9a3089b70dc20a71c60f23
Julianne Sweeten <juliannesweeten@gmail.com>
2025-03-21 16:30:23 -0400

    Update script.py

--- lab5/script.py
+++ lab5/script.py
@@ -2,7 +2,6 @@
 import json
 import csv
 
-print("first few steps done")
 
 with open('data/schacon.repos.json', 'r') as file:
     data = json.load(file)
@@ -16,12 +15,10 @@ for repo in data:
                 repo.get("visibility", "")
         ])
 
-print(f"extraction of {len(ext_data)} repos.")
 
 csv_filename = "chacon.csv"
 
 with open(csv_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["name", "html_url", "updated_at", "visibility"])
-        writer.writerows(ext_data[:5])
-print(f"CSV file '{csv_filename}' made")
+        writer.writerows(ext_data[:5]) 
